2f2455dda39c27ce40b2fb085e44a75cf44a8ec7
Mariam-Abdou <126914458+Mariam-Abdou@users.noreply.github.com>
2025-04-27 17:05:43 +0300

    edit queue name

--- crawler_object.py
+++ crawler_object.py
@@ -3,7 +3,7 @@ from crawler_node import Crawler
 crawler = Crawler(
     crawler_id="crawler-1",
     crawler_queue="https://sqs.us-east-1.amazonaws.com/111111111111/CrawlerQueue",
-    master_queue="https://sqs.us-east-1.amazonaws.com/111111111111/MasterQueue",
+    master_queue="https://sqs.us-east-1.amazonaws.com/111111111111/ReportQueue",
     indexer_queue="https://sqs.us-east-1.amazonaws.com/111111111111/IndexerQueue",
     s3_bucket="your-s3-bucket-name",
     dynamodb_table="CrawlerHeartbeatTable",
