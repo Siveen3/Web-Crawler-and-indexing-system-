081c7e6dfbf2bdc72c4ab3240f9b51fba654293b
Mariam-Abdou <126914458+Mariam-Abdou@users.noreply.github.com>
2025-05-01 20:03:34 +0300

    Client interface

--- crawler_object.py
+++ crawler_object.py
@@ -1,11 +1,11 @@
-from crawler_node import Crawler
+from crawler import Crawler
 
 crawler = Crawler(
     crawler_id="crawler-1",
     crawler_queue="https://sqs.us-east-1.amazonaws.com/138749495090/CrawlQueue",
     master_queue="https://sqs.us-east-1.amazonaws.com/138749495090/ReportQueue",
     indexer_queue="https://sqs.us-east-1.amazonaws.com/-------------/IndexerQueue",
-    s3_bucket="your-s3-bucket-name",
+    s3_bucket="crawler-indexer-bucket",
     dynamodb_table="CrawlerHeartbeatTable",
 )
 
