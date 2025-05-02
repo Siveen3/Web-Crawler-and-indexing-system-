c08af008cef4b8c965866bb287ed927872465e67
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2025-05-02 16:07:16 +0300

    add domain

--- master.py
+++ master.py
@@ -13,59 +13,36 @@ class DecimalEncoder(json.JSONEncoder):
 
 class MasterNode:
     def __init__(self, region_name, crawl_queue_url, report_queue_url, heartbeat_table_name,
-                  task_table_name, dead_letter_queue_url, blocked_table_name, index_feedback_queue_url,
-                  index_status_table_name, ResponseQueue, request_queue_url,search_queue_url, max_depth=2):
-        self.region_name = region_name
-        self.crawl_queue_url = crawl_queue_url
-        self.report_queue_url = report_queue_url
-        self.heartbeat_table_name = heartbeat_table_name
-        self.task_table_name = task_table_name
-        self.dead_letter_queue_url = dead_letter_queue_url
-        self.index_feedback_queue_url = index_feedback_queue_url
-        self.index_status_table_name = index_status_table_name
-        self.ResponseQueue = ResponseQueue
-        self.max_depth = max_depth
-        self.request_queue_url = request_queue_url
-        self.sqs = boto3.client('sqs', region_name=self.region_name)
-        self.dynamodb = boto3.resource('dynamodb', region_name=self.region_name)
-        self.heartbeat_table = self.dynamodb.Table(self.heartbeat_table_name)
-        self.task_table = self.dynamodb.Table(self.task_table_name)
-        self.blocked_table = self.dynamodb.Table(blocked_table_name)
-        self.index_status_table = self.dynamodb.Table(self.index_status_table_name)
-        self.search_queue_url = search_queue_url
-
-        logging.basicConfig(filename='master_log.log', level=logging.INFO,
-                            format='%(asctime)s [%(levelname)s] %(message)s')
-
         self.TIMEOUT_SECONDS = 120
 
-    def send_url_to_crawl_queue(self, url, depth=0):
-    if self.is_blocked_url(url):
-        logging.warning(f"[Master] Skipping blocked URL: {url}")
-        return
-    
-    assigned_at = datetime.now(timezone.utc).isoformat()
-    message = {
-        "url": url,
-        "depth": depth,
-        "max_depth": self.max_depth,
-        "assigned_at": assigned_at  # Include assigned_at in the message
-    }
-    self.sqs.send_message(
-        QueueUrl=self.crawl_queue_url,
-        MessageBody=json.dumps(message, cls=DecimalEncoder)
-    )
-    logging.info(f"[Master] Sent URL to CrawlQueue: {url} (depth={depth})")
-
-    self.task_table.put_item(
-        Item={
-            'url': url,
-            'assigned_at': assigned_at,  # Always include assigned_at
-            'depth': depth,
-            'status': 'pending',
-            'retries': 0
+    def send_url_to_crawl_queue(self, url, domain, depth=0):
+        if self.is_blocked_url(url):
+            logging.warning(f"[Master] Skipping blocked URL: {url}")
+            return
+        
+        assigned_at = datetime.now(timezone.utc).isoformat()
+        message = {
+            "url": url,
+            "depth": depth,
+            "domain": domain,
+            "assigned_at": assigned_at  # Include assigned_at in the message
         }
-    )
+        self.sqs.send_message(
+            QueueUrl=self.crawl_queue_url,
+            MessageBody=json.dumps(message, cls=DecimalEncoder)
+        )
+        logging.info(f"[Master] Sent URL to CrawlQueue: {url} (depth={depth})")
+    
+        self.task_table.put_item(
+            Item={
+                'url': url,
+                'assigned_at': assigned_at,  # Always include assigned_at
+                'depth': depth,
+                'domain': domain
+                'status': 'pending',
+                'retries': 0
+            }
+        )
        
     def monitor_client_requests(self):
         logging.info("[Monitor] Listening for client requests...")
@@ -254,14 +231,14 @@ class MasterNode:
                 status = body.get('status', 'unknown')
                 error = body.get('error', '')
                 assigned_at = body.get('assigned_at')
-
+                domain = body.get('domain, 'none')
                 key = {'url': crawled_url, 'assigned_at': assigned_at} 
 
                 if status == 'success':
                     logging.info(f"[{crawler_id}] Successfully crawled: {crawled_url} at depth {depth}")
                     if depth < self.max_depth:
                         for url in extracted_urls:
-                            self.send_url_to_crawl_queue(url, depth=depth+1)
+                            self.send_url_to_crawl_queue(url,domain, depth=depth+1)
                     self.task_table.update_item(
                         Key=key,
                         UpdateExpression="SET #s = :s",
@@ -339,11 +316,12 @@ class MasterNode:
             retries = item.get('retries', 0)
             url = item['url']
             depth = item['depth']
+            domain = item['domain']
 
             if (now - datetime.fromisoformat(assigned_at)).total_seconds() > self.TIMEOUT_SECONDS:
                 if retries < 3:
                     logging.warning(f"[Timeout] Requeuing stale task: {url}")
-                    self.send_url_to_crawl_queue(url, depth=depth)
+                    self.send_url_to_crawl_queue(url, domain, depth=depth)
                     # Delete old item
                     self.task_table.delete_item(Key={'url': url, 'assigned_at': assigned_at})
                     # Put new item with updated assigned_at and incremented retries
