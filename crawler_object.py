ab1f35ede5d65c36fb8f4dd88203db81e2056050
Mariam-Abdou <126914458+Mariam-Abdou@users.noreply.github.com>
2025-05-02 15:21:06 +0300

    Update crawler_object.py

--- crawler_object.py
+++ crawler_object.py
@@ -2,9 +2,9 @@ from crawler import Crawler
 
 crawler = Crawler(
     crawler_id="crawler-1",
-    crawler_queue="https://sqs.us-east-1.amazonaws.com/138749495090/CrawlQueue",
-    master_queue="https://sqs.us-east-1.amazonaws.com/138749495090/ReportQueue",
-    indexer_queue="https://sqs.us-east-1.amazonaws.com/138749495090/IndexQueue",
+    crawler_queue_url="https://sqs.us-east-1.amazonaws.com/138749495090/CrawlQueue",
+    master_queue_url="https://sqs.us-east-1.amazonaws.com/138749495090/ReportQueue",
+    indexer_queue_url="https://sqs.us-east-1.amazonaws.com/138749495090/IndexQueue",
     s3_bucket="crawler-indexer-bucket",
     dynamodb_table="CrawlerHeartbeatTable",
 )
