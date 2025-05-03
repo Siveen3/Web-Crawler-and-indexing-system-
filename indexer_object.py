1b767dc0f7e95e799d73308c2520ab8158af1548
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2025-05-04 01:03:36 +0300

    Open search Connection

--- indexer_object.py
+++ indexer_object.py
@@ -1,28 +1,29 @@
 from indexer import Indexer
-from elasticsearch import Elasticsearch
+from opensearchpy import OpenSearch
 import certifi
 
-host = "https://vpc-opensearch-domain-4xiamr7tymr4563vc4hxuvszia.us-east-1.es.amazonaws.com"
+host = "vpc-opensearch-domain-4xiamr7tymr4563vc4hxuvszia.us-east-1.es.amazonaws.com"
 port = 443
-username = "Dist"
-password = "2004#Dist" 
+username = "dist"
+password = "Hager#2004" 
 
-es = Elasticsearch(
+
+es = OpenSearch(
     hosts=[{'host': host, 'port': port}],
     http_auth=(username, password),
     use_ssl=True,
     verify_certs=True,
     ca_certs=certifi.where()
-    )
+)
 
 indexer = Indexer(es=es,
-                  indexer_id="indexer_1",
                   index_name="content_index", 
                   dynamodb_table="CrawlerHeartbeatTable", 
                   s3_bucket="crawler-indexer-bucket", 
                   content_queue_url='https://sqs.us-east-1.amazonaws.com/138749495090/IndexQueue', 
                   search_queue_url='https://sqs.us-east-1.amazonaws.com/138749495090/SearchQueue', 
-                  response_queue_url='https://sqs.us-east-1.amazonaws.com/138749495090/FeedbackQueue'
+                  response_queue_url='https://sqs.us-east-1.amazonaws.com/138749495090/FeedbackQueue',
+                  indexer_id = 'indexer_id'
                   )
 
 
